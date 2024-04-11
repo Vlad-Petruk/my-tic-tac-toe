6d34ca8a4da65b75e2880d8318768d16336986eb
Vlad-Petruk <vladpetruk00@gmail.com>
2024-04-11 19:28:55 +0300

    add Game factory function(iife)
    with testing functionality to play in console

--- main.js
+++ main.js
@@ -9,9 +9,15 @@ const Gameboard = (()=> {
         return gameboard = new Array(9).fill('');
     }
 
+    const winningCombinations = [
+        [1,2,3], [2,3,4], [3,4,5],
+        [4,5,6], [6,7,8], [7,8,9]
+    ]
+
     return {
         getBoard,
-        clearBoard
+        clearBoard,
+        winningCombinations
     }
 })();
 
@@ -20,9 +26,50 @@ function createPlayer(name, marker) {
 }
 
 
-
 const DisplayController = (()=> {
 
 })();
 
-console.log(Gameboard)
+
+const Game = (() => {
+    const playerX = createPlayer('playerX', 'X');
+    const playerO = createPlayer('playerO', 'O');
+    const board = Gameboard.getBoard();
+    let currentPlayer = playerX;
+
+    function changeTurn() {
+        return (currentPlayer === playerX) ? playerO : playerX;
+    }
+
+    function markSpot(index) {
+        if (index < 0 || index > 8) {
+            console.log ('Invalid index')
+        } else {
+            if (board[index] === '') {
+                board[index] = currentPlayer.marker;
+                currentPlayer = changeTurn();
+            } else console.log('Invalid spot')
+        }
+    }
+
+    function endGame() {
+        Gameboard.clearBoard();
+    }
+
+    return { 
+        markSpot,
+        endGame
+    }
+})();
+
+
+Game.markSpot(8);
+Game.markSpot(3);
+Game.markSpot(12);
+Game.markSpot(0);
+Game.markSpot(0);
+
+
+
+
+console.log(Gameboard.getBoard())
