8082387bd8a8741b77f9d0e49641cabd3766fece
Vlad-Petruk <vladpetruk00@gmail.com>
2024-04-12 14:10:16 +0300

    add some styling of the gameboard

--- main.js
+++ main.js
@@ -10,9 +10,10 @@ const Gameboard = (()=> {
     }
 
     const winningCombinations = [
-        [0,1,2], [1,2,3], [2,3,4], 
-        [3,4,5], [4,5,6], [6,7,8],
-    ]
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], 
+        [0, 4, 8], [2, 4, 6]             
+    ];
 
     return {
         getBoard,
@@ -35,7 +36,7 @@ const DisplayController = (()=> {
             const box = document.createElement('div');
             box.classList.add('board-box') 
             box.id = `box${i}`;
-            box.textContent = `Box ${i} ${board[i]}`
+            box.textContent = `${board[i]}`
 
             boardBox.append(box)
         }
